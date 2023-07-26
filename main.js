37638131859471883669c1db8351b972570c778b
Fima1716 <efimt74@gmail.com>
2023-07-26 14:23:59 +0300

    calculator 2.0
    now you can calculate expressions and they'll be displayed on calculator's display

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+const display = document.getElementById("result");
+const buttons = document.querySelectorAll('input[type="button"]');
+const equal = document.querySelector(".equal");
+const del = document.querySelector(".delete");
+const allClear = document.querySelector(".clear");
 
+function updateDisplay(value) {
+  display.textContent += value;
+}
 
-const resultElement = document.getElementById('result')
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const currentValue = button.value;
+    updateDisplay(currentValue);
+  });
+});
+
+function math(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+
+    default:
+      return "Ошибка";
+  }
+}
+
+function equasion(string) {
+  let [a, operation, b] = string.split(/(\+|\-|\*|\/)/);
+  a = parseFloat(a);
+  b = parseFloat(b);
+  return math(a, b, operation);
+}
+
+equal.addEventListener("click", (event) => {
+  display.textContent = equasion(display.textContent);
+});
+
+del.addEventListener("click", (event) => {
+  display.textContent = display.textContent.slice(0, -3);
+});
+
+allClear.addEventListener("click", (event) => {
+  display.textContent = "";
+});
